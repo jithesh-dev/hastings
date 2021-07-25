9a8e6935186da0fd430227b6f7548bb5f26d603a
jitheshkumar <jitheshkumar@hollandandbarrett.com>
2021-07-25 19:51:52 +0100

    feat: initial commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Yejjjaathi vand</h1>This is a React dsddf sf sdf sdfs
+    </div>
+  );
+};
+
+export default App;
